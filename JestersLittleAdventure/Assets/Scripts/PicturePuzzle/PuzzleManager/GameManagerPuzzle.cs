a1df680a312f80d953bc31fcf6a80fc0ac70f289
ALostdragon <149062492+ALostdragon@users.noreply.github.com>
2024-01-27 15:09:31 +0100

    more updates

--- JestersLittleAdventure/Assets/Scripts/PicturePuzzle/PuzzleManager/GameManagerPuzzle.cs
+++ JestersLittleAdventure/Assets/Scripts/PicturePuzzle/PuzzleManager/GameManagerPuzzle.cs
@@ -8,11 +8,14 @@ public class GameManagerPuzzle : MonoBehaviour
 
     [SerializeField] private GameObject PrefabPieces;
     [SerializeField] private Vector3 StartPosition;
-
     private int emptySpace;
-    [Header("exclemation factorial number")]
+
+    private List<Transform> pieces;
+
+    [Header("lenght and width of puzzle")]
     [SerializeField] private int size = 3;
     // the x and y lenght is changeable on size. (currently not supporting different numbers for now.)
+
     void Start()
     {
         createPuzzlePiece(0.1f);
@@ -25,10 +28,9 @@ public class GameManagerPuzzle : MonoBehaviour
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
             if (hit)
             {
-                //position told by index
-                for (int i = 0; i < ?.Count; i++)
+                for (int i = 0; i < pieces.Count; i++)
                 {
-                    if (?[i] == hit.transform)
+                    if (pieces[i] == hit.transform)
                     {
                         if (SwapIfValid(i, -size, size)) { break; }
                         if (SwapIfValid(i, +size, size)) { break; }
@@ -43,22 +45,20 @@ public class GameManagerPuzzle : MonoBehaviour
     private void createPuzzlePiece(float gapboarder)
     {
         float width = 1 / (float)size;
-        for (int row = 0; row < size; row++) //row of image piece
+        for (int row = 0; row < size; row++) //row of image pieces
         {
-            for (int col = 0; col < size; col++) //collumn of image piece
+            for (int col = 0; col < size; col++) //collumn of image pieces
             {
                 var puzzlePiece = Instantiate(PrefabPieces, StartPosition, Quaternion.identity);
-
                 puzzlePiece.transform.localPosition = new Vector3((2 * width * col) + width,
-                                                        (2 * width * row) - width, 0);
+                                                                  (2 * width * row) - width, 0);
                 puzzlePiece.transform.localScale = ((2 * width) - gapboarder) * Vector3.one;
-
+                //goes right from bottom left at each collumn
                 if ((row == size - 1) && (col == size - 1))
                 {
                     emptySpace = (size * size) - 1;
                     puzzlePiece.gameObject.SetActive(false);
                 }
-
             }
         }
     }
@@ -67,10 +67,10 @@ public class GameManagerPuzzle : MonoBehaviour
     {
         if (((i % size) != colCheck) &&((i + offset) == emptySpace))
         {
-            (?[i],?[i + offset]) + (?[i + offset], ?[i]);
+            //I am im deep PASTAMANIA!!!
 
-            //update empty space
             emptySpace = i;
+            //update empty space
             return true;
         }
         return false;
