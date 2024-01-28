61167bd96487063840584823c9f5b65a976f8365
Marc Biegota <marc.biegota@web.de>
2024-01-28 14:48:11 +0100

    new puzzle implementation

--- JestersLittleAdventure/Assets/Scripts/PicturePuzzleBiegoMar/TilePositionItem.cs
+++ JestersLittleAdventure/Assets/Scripts/PicturePuzzleBiegoMar/TilePositionItem.cs
@@ -5,6 +5,7 @@ namespace PicturePuzzleBiegoMar
 {
     public class TilePositionItem
     {
+        public string Tag { get; set; }
         public Vector3 RelativePosition { get; set; }
         [CanBeNull] public SpriteRenderer Tile { get; set; }
     }
